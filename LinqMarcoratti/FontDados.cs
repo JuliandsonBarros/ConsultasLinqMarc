e9812a3e6f1b1f5348156b6533d9eb667f159b87
JULIANDSON BARROS SOARES <juliandson.soares@mpm.mp.br>
2023-11-22 16:51:12 -0300

    Consultas Linq curso Marcoratti

---
+++ LinqMarcoratti/FontDados.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqMarcoratti
+{
+    class FontDados
+    {
+        public static List<Aluno> GetAluno()
+        {
+            List<Aluno> alunos = new()
+            {
+                new Aluno {Nome = "Fulano de Tal",Idade = 20,  Nota = 5, Cursos = new List<string> { "Java, Angular" } },
+                new Aluno {Nome = "Cicrano de Tal",Idade = 20,  Nota = 4, Cursos = new List<string> { ".NET, React" } },
+                new Aluno {Nome = "Beltrano de Tal",Idade = 20,  Nota = 5, Cursos = new List<string> { "VB net, Botstrap" } },
+                new Aluno {Nome = "Neymar de Tal",Idade = 32,  Nota = 2, Cursos = new List<string> { "JavaScript, Boststrap" } },
+                new Aluno {Nome = "Messi de Tal",Idade = 36,  Nota = 5, Cursos = new List<string> { "PHP, Angular" } },
+                new Aluno {Nome = "Cristiano Ronaldo de Tal",Idade = 37,  Nota = 5, Cursos = new List<string> { "sQL, Angular" } },
+                new Aluno {Nome = "Ronaldo de Tal",Idade = 48,  Nota = 4, Cursos = new List<string> { "PYTON, Angular" } },
+                new Aluno {Nome = "Hulk de Tal",Idade = 28,  Nota = 5, Cursos = new List<string> { "Assembly, Html" } },
+                new Aluno {Nome = "Jack de Tal",Idade = 30,  Nota = 5, Cursos = new List<string> { "Java, Angular" } },
+            };
+            return alunos;
+        }
+
+        public static List<Funcionario> GetFuncionarios()
+        {
+            List<Funcionario> Funcionarios = new()
+            {
+                new Funcionario {Nome = "Juliandson Barros Soares",Idade = 37, Salario = 10000.00 },
+                new Funcionario {Nome = "Eduarda Leão Galvão Barros",Idade =  18,Salario = 8000.00 },
+                new Funcionario {Nome = "Bruna Leão Galvão Barros",Idade =  18,Salario = 8000.00 },
+                new Funcionario {Nome = "Isabelle da Paz Barros",Idade =  19,Salario = 8000.00 }
+            };
+            return Funcionarios;
+        }
+
+        public static int[] GetIdades() {
+
+            var idades = new[] { 10, 20, 30,30, 40, 50, 60, 70, 80,80, 90 };
+            return idades;
+        }
+
+        public static string[] GetNomes() {
+
+            var nomes = new[] { "Messi", "Cristiano Ronaldo", "Salah", "salah", "sala"};
+            return nomes;
+        }
+
+        public static string[] GetPaises() {
+
+            var paises = new[] { "Brasil", "China", "Estados Unidos", "França", "Espanha", "Japão" };
+            return paises;
+        }
+        public static string[] GetPaises2() {
+
+            var paises2 = new[] { "Bolívia", "Equador", "Argentina", "Chile", "Paraguai", "Peru" };
+            return paises2;
+        }
+
+        public static string[] GetAlunosReprovados() {
+
+            var reprovados = new[] { "Fulano de tal", "Neymar", "Jack de tal" };
+            return reprovados;
+        }
+    }
+}
---
+++ LinqMarcoratti/Program.cs
@@ -0,0 +1,105 @@
+﻿using LinqMarcoratti;
+
+Console.WriteLine("Consultas LINQ");
+
+IEnumerable<List<string>> retornoListaAluno = FontDados.GetAluno().Select(c => c.Cursos).ToList();
+
+foreach (var retornoLista in retornoListaAluno) 
+{
+    foreach (var curso in retornoLista)
+    {
+        Console.WriteLine(curso);   
+       
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("====Resultado Distinct=====");
+
+var result = FontDados.GetIdades().Distinct().ToList();
+
+foreach (var idade in result) 
+{
+    Console.Write(" " + idade);
+}
+
+Console.WriteLine();
+
+var result2 = FontDados.GetNomes().Distinct(StringComparer.OrdinalIgnoreCase).ToList();
+
+foreach (var nome in result2)
+{ 
+    Console.Write("/" + nome);
+}
+
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("====Resultado DistinctBy=====");
+
+var result3 =  FontDados.GetAluno().ToList();
+
+var alunosDistintos = result3.DistinctBy(a => a.Idade);
+
+foreach (var idadeDistintas in alunosDistintos)
+{
+    Console.WriteLine("O aluno: " + idadeDistintas.Nome + " tem a ideade de: " + idadeDistintas.Idade);
+}
+
+Console.WriteLine();
+Console.WriteLine("====Resultado EceptBy=====");
+
+var result4 = FontDados.GetAlunosReprovados().Distinct().ToList();
+
+foreach (var reprovado in result4) { 
+    Console.WriteLine(reprovado);
+}
+
+Console.WriteLine();
+Console.WriteLine("====Resultado OrderBy em ordem Crescente=====");
+
+var result5 = FontDados.GetAluno().OrderBy(a => a.Nome).ToList();
+
+foreach (var nomeAluno in result5)
+{
+    Console.WriteLine(nomeAluno.Nome);
+}
+
+Console.WriteLine();
+Console.WriteLine("=========Ordem Decrescente=========");
+
+var result6 = FontDados.GetAluno().OrderByDescending(a => a.Nome).ToList();
+
+foreach (var nomeAluno in result6) {
+    Console.WriteLine(nomeAluno.Nome);
+}
+
+Console.WriteLine();
+Console.WriteLine("=========Ordem Crescente com dois criterios=========");
+
+var result7 =  FontDados.GetAluno().OrderBy(a => a.Nome).ThenBy(a => a.Idade).ToList();
+
+foreach (var nomeAluno in result7) { 
+    Console.WriteLine(nomeAluno.Nome + " " + nomeAluno.Idade);
+}
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine();
+Console.WriteLine("=========Consulta que retorna Alunos C/ idades superiores a 30 anos=========");
+
+var result8 = FontDados.GetAluno().Where(i => i.Idade >= 30).OrderBy(a => a.Nome).ToList();
+
+foreach (var nomeAluno in result8) {
+    Console.WriteLine(nomeAluno.Nome + " " +  nomeAluno.Idade);
+};
+
+
+Console.ReadKey();
